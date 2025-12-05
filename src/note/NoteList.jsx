1cbdf1d1eee7af17fab45b55dead9a388ab29c26
akmalfman <akmalfsalman2@gmail.com>
2025-12-05 10:24:38 +0700

    implement memo on Note Feat

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,17 +1,33 @@
-import { useContext } from "react";
+import { useContext, useMemo, useRef, useState } from "react";
 import Note from "./Note.jsx";
 import { NoteContext } from "./NoteContext.jsx";
 
 export default function NoteList() {
     const notes = useContext(NoteContext);
+    const [search, setSearch] = useState("");
+    const searchInput = useRef(null)
+
+    const filteredNotes = useMemo(() => {
+        console.info("Filtering Notes")
+        return notes.filter(note => note.text.includes(search))
+    }, [notes, search]);
+
+    function handleSearch() {
+        console.info("Search Handle")
+        setSearch(searchInput.current.value);
+    }
 
     return (
-        <ul>
-            {notes.map(note => (
-                <li key={note.id}>
-                    <Note note={note} />
-                </li>
-            ))}
-        </ul>
+        <div>
+            <input type="text" placeholder="Search" ref={searchInput} />
+            <button onClick={handleSearch}>Search</button>
+            <ul>
+                {filteredNotes.map(note => (
+                    <li key={note.id}>
+                        <Note note={note} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
